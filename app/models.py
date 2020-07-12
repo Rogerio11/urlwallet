bbbcf6baea5009584da28c4aa41964feacb88878
Rogerio11 <55982331+Rogerio11@users.noreply.github.com>
2020-07-12 06:38:04 +0200

    onglet - dossier - accueil done " just form create update to do"

--- app/models.py
+++ app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Lien(models.Model):
+    lien_url = models.CharField(max_length = 700)
+    info = models.CharField(max_length = 700)
+    
+    class Meta:
+        db_table = 'lien'
+        
+class Dossier(models.Model):
+    nom = models.CharField(max_length = 700)
+    info = models.CharField(max_length = 700)
+    pere_id = models.IntegerField(default= -1)
+    class Meta:
+        db_table = 'dossier'
+    
+class Onglet(models.Model):
+    nom = models.CharField(max_length = 50)
+    class Meta:
+        db_table = 'onglet'
+        
+class Assoongletdossier(models.Model):
+    onglet = models.ForeignKey(Onglet, on_delete=models.CASCADE)
+    dossier = models.ForeignKey(Dossier, on_delete=models.CASCADE)
+    
+class Assoongletlien(models.Model):
+    onglet = models.ForeignKey(Onglet, on_delete=models.CASCADE)
+    lien = models.ForeignKey(Lien, on_delete=models.CASCADE)
+    
+class Assodossierlien(models.Model):
+    dossier = models.ForeignKey(Dossier, on_delete=models.CASCADE)
+    lien = models.ForeignKey(Lien, on_delete=models.CASCADE)
+    
--- app/views.py
+++ app/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from app.models import Assodossierlien, Assoongletdossier, Assoongletlien, Dossier, Onglet
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def to_and(value):
+    return value.replace("&","and")
 
 # Create your views here.
+def home(request):    
+    return render(request,"index.html")
+
+
+def test(request):
+    onglets = Onglet.objects.all()
+    dico = {}
+    dicoTest = {}
+    i = 0
+    for ong in onglets:
+        dico[ong.nom] = i
+        onglet_dossiers = Assoongletdossier.objects.filter(onglet = ong)
+        onglet_liens = Assoongletlien.objects.filter(onglet = ong)
+        tabDossier = []
+        tabLien = []
+        for ong_dos in onglet_dossiers:
+            tabDossier.append(ong_dos.dossier)
+        for ong_lien in onglet_liens:
+            tabLien.append(ong_lien.lien)
+        dicoTest[ong.nom] = {'num' : i, 'dossiers' : tabDossier, 'liens' : tabLien}
+        i+=1
+    return render(request, "test.html",locals())
+
+def foncaccueil(request):
+    onglets = Onglet.objects.all()
+    dico = {}
+    i = 0
+    for ong in onglets:
+        onglet_dossiers = Assoongletdossier.objects.filter(onglet = ong)
+        onglet_liens = Assoongletlien.objects.filter(onglet = ong)
+        tabDossiers = []
+        tabLiens = []
+        for ong_dos in onglet_dossiers:
+            tabDossiers.append(ong_dos.dossier)
+        for ong_lien in onglet_liens:
+            tabLiens.append(ong_lien.lien)
+        dico[ong] = {'num' : i, 'dossiers' : tabDossiers, 'liens' : tabLiens}
+        i+=1
+    return render(request, "accueil.html",locals())
+
+def testonglet(request, nom_onglet):
+    onglets = Onglet.objects.all()
+    dico = {}
+    i = 0
+    nom_ong = nom_onglet.replace("-", "_")
+    for ong in onglets:
+        dico[ong.nom] = i
+        i+=1
+    chaine = " un deux trois"
+
+    return render(request, "test_onglet.html",locals())
+
+def fonconglet(request, idOnglet):
+    onglets = Onglet.objects.all()
+    tabDossiers = []
+    tabLiens = []
+    
+    currentOnglet = get_object_or_404(Onglet, pk = idOnglet)
+    onglet_dossiers = Assoongletdossier.objects.filter(onglet = currentOnglet)
+    onglet_liens = Assoongletlien.objects.filter(onglet = currentOnglet)
+    
+    for ong_dos in onglet_dossiers:
+        tabDossiers.append(ong_dos.dossier)
+    for ong_lien in onglet_liens:
+        tabLiens.append(ong_lien.lien)
+
+    return render(request, "onglet.html",locals())
+
+def foncdossier(request, idOnglet, idDossier):
+    currentOnglet = get_object_or_404(Onglet, pk = idOnglet)
+    currentDossier = get_object_or_404(Dossier, pk = idDossier)
+    onglets = Onglet.objects.all()
+    tabDossiers = Dossier.objects.filter(pere_id = idDossier)
+    tabLiens = []
+    
+    dossier_liens = Assodossierlien.objects.filter(dossier = currentDossier)
+    
+    for dos_lie in dossier_liens:
+        tabLiens.append(dos_lie.lien)
+        
+    return render(request, "dossier.html", locals())
